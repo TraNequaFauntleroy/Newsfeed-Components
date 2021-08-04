30ea9fd9d81acc66fa1b925d745aca1f5dc67a06
NeNeTheCreative <shecodes@nenethecreative.com>
2021-08-04 18:53:35 -0400

    menu almost completed. working out a couple bugs

--- components/Menu.js
+++ components/Menu.js
@@ -18,9 +18,59 @@ let menuItems = [
     </ul>
   </div>
 
-  The 'menuMaker' takes an array of menu items as its only argument.
+  The 'menuMaker' takes an array of menu items as its only argument.*/
 
-  Step 2: Inside the function, iterate over the array creating a list item <li> element for each item in the array.
+function menuMaker(array) {
+  //create elements
+  const menuDiv = document.createElement('div');
+  const uList = document.createElement('ul');
+  const students = document.createElement('li');
+  const faculty = document.createElement('li');
+  const news = document.createElement('li');
+  const tech = document.createElement('li');
+  const music = document.createElement('li');
+  const logOut = document.createElement('li');
+
+  //add class name
+  menuDiv.classList.add('menu');
+
+  // assemble elements
+  uList.appendChild(menuDiv);
+  students.appendChild(uList); 
+  faculty.appendChild(uList);  
+  news.appendChild(uList);  
+  tech.appendChild(uList);
+  music.appendChild(uList);
+  logOut.appendChild(uList);
+
+  //find element
+  const menuButton = document.querySelector('.menu-button');
+
+  // adding content
+  students.textContent = array[0];
+  faculty.textContent = array[1];
+  news.textContent = array[2];
+  tech.textContent = array[3];
+  music.textContent = array[4];
+  logOut.textContent = array[5];
+
+  // adding event listener
+  menuButton.addEventListener('click', event => {
+    menuDiv.classList.toggle('.menu--open')
+  });
+
+  return menuDiv;
+}
+
+const header = document.querySelector('.header');
+const newItem = menuMaker(array);
+
+newItem.map(item => {
+  menuItems.appendChild(header);
+})
+  
+
+/* Step 2: Inside the function, iterate over the array creating a list item <li> element for each item in the array.
   Add those items to the <ul>
 
   Step 3: Still inside your function, select from the DOM the menu button (the element with a class of 'menu-button').
@@ -31,3 +81,7 @@ let menuItems = [
 
   Step 6: Use 'menuMaker' to create a menu using the 'menuItems' array, and append the returned menu to the header.
 */
+
+
+
+
